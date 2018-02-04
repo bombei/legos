cebcb297ebb49cf252375d21437aa1feea4d87c4
Miguel Teixeira <miguelteixeira@domdigital.com>
2018-02-04 21:40:40 +0000

    adding server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
- var app = express();
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+
 
  app.get('/', (req, response) => {
 
  });
 
- app.listen(3000);
+ app.listen(port, () => {
+   console.log(`Server running on port ${port}`);
+ });

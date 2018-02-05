839722010c8b783ebcfabe313142026731434720
Miguel Teixeira <miguelteixeira@domdigital.com>
2018-02-05 13:52:27 +0000

    add GET command

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./../db/mongoose.js');
 var {Peca} = require('./../models/peca.js');
+var {ObjectID} = require('mongodb');
 
 const port = process.env.PORT || 3000;
 var app = express();
@@ -30,6 +31,26 @@ app.get('/peca', (req, res) => {
       res.status(400).send(e);
    })
 });
+
+app.get('/peca/:id', (req, res) => {
+   var id = req.params.id;
+   if (!ObjectID.isValid(id)){
+      return res.status(404).send();
+   }
+
+   Peca.findById(id).then((peca) => {
+      if(!peca){
+         return res.status(404).send();
+      }
+      res.send({peca});
+   }).catch((e) => {
+      res.status(400).send(e);
+   })
+
+}, (e) => {
+
+})
+
  app.listen(port, () => {
    console.log(`Server running on port ${port}`);
  });

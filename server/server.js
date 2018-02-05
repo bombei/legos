a9a44d8a8fa6db88d585ad1c54ab37ced68fa250
Miguel Teixeira <miguelteixeira@domdigital.com>
2018-02-05 14:17:03 +0000

    add delete and update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 
 var {mongoose} = require('./../db/mongoose.js');
 var {Peca} = require('./../models/peca.js');
@@ -66,7 +67,21 @@ app.delete('/peca/:id', (req, res) =>{
 });
 
 app.patch('/peca/:id', (req, res) => {
+   var id = req.params.id;
+   if (!ObjectID.isValid(id)){
+      return res.status(404).send();
+   }
 
+   var body = _.pick(req.body, ['codigo', 'cor', 'tamanho']);
+
+   Peca.findByIdAndUpdate(id, {$set: body}, {new: true}).then((peca) => {
+      if (!peca){
+         return res.status(404).send();
+      }
+      res.send({peca});
+   }).catch((e) => {
+      res.status(400).send(e);
+   });
 });
 
  app.listen(port, () => {
